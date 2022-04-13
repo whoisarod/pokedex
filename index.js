9fc7a7be060045f6b2ad25a0b391268efbc1dc0f
Angel Rodriguez <angelrodri@hotmail.com>
2022-04-13 12:22:19 -0700

    updated js and css files to fetch all pokemon data and display

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ async function getAllPokemon() {
     return data    
 }
 
-getAllPokemon().then(allPokemon => {
-    console.log(allPokemon[0])
-})
+function getPokemonHTML(aPokemon) {
+    return `
+        <div class="a-pokemon">
+            <div class="a-pokemon-id">${aPokemon.id}</div>
+            <div class="a-pokemon-name">${aPokemon.name.english}</div>
+            <div class="a-pokemon-type">${aPokemon.type.join(' / ')}</div>
+
+            <div class="a-pokemon-stat">HP: ${aPokemon.base.HP}</div>
+            <div class="a-pokemon-stat">Attack: ${aPokemon.base.Attack}</div>
+            <div class="a-pokemon-stat">Defense: ${aPokemon.base.Defense}</div>
+            <div class="a-pokemon-stat">Speed: ${aPokemon.base.Speed}</div>
+
+            <div class="a-pokemon-alt-name">${aPokemon.name.japanese}</div>
+            <div class="a-pokemon-alt-name">${aPokemon.name.chinese}</div>
+            <div class="a-pokemon-alt-name">${aPokemon.name.french}</div>
+        </div>
+`
+}
+
+function displayPokedex(allPokemon) {
+    document.body.innerHTML = `<div class="my-pokedex">
+        ${allPokemon.map(getPokemonHTML).join('')}
+    </div>`
+}
+
+getAllPokemon().then(displayPokedex)
